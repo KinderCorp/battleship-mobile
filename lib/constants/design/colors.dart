b2b9b9f4839e545c80b63f1ad91e2c8ea72d2181
Alexandra Farina <alexandra.farina@mmibordeaux.com>
2023-03-03 16:27:38 +0100

    add color and font managment

---
+++ lib/constants/design/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class BSColors {
+  Color red = const Color(0xFF820000);
+  Color brown = const Color(0xFF873100);
+  Color darkOrange = const Color(0xFFF39221);
+  Color orange = const Color(0xFFF7B623);
+  Color yellow = const Color(0xFFF9D722);
+  Color darkPurple2 = const Color(0xFF25143C);
+  Color darkPurple = const Color(0xFF3A1D62);
+  Color purple = const Color(0xFF7E29C2);
+  Color lightPurple = const Color(0xFFA452DA);
+  Color pink = const Color(0xFFE290FF);
+  Color black = const Color(0xFF1C1C1C);
+  Color darkGrey2 = const Color(0xFF636363);
+  Color darkGrey = const Color(0xFF808080);
+  Color grey = const Color(0xFFA9A9A9);
+  Color lightGrey = const Color(0xFFEEEEEE);
+  Color white = const Color(0xFFFFFFFF);
+}
---
+++ lib/constants/design/font.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BattleShipFont {
+  static TextTheme policy = TextTheme(
+    titleLarge: GoogleFonts.roboto(textStyle:const TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w700,
+      letterSpacing: 0,
+    )),
+    titleMedium: GoogleFonts.roboto(textStyle:const TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.w700,
+      letterSpacing: 0,
+    )),
+    titleSmall: GoogleFonts.roboto(textStyle:const TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0,
+    )),
+    bodyLarge: GoogleFonts.roboto(textStyle:const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 0,
+    )),
+    bodyMedium: GoogleFonts.roboto(textStyle:const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0,
+    )),
+    bodySmall: GoogleFonts.roboto(textStyle:const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0,
+    )),
+    labelLarge: GoogleFonts.roboto(textStyle:const TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.w700,
+      letterSpacing: 0,
+    )),
+    labelMedium: GoogleFonts.roboto(textStyle:const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+      letterSpacing: 0,
+    )),
+    labelSmall: GoogleFonts.roboto(textStyle:const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0,
+    )),
+  );
+}
